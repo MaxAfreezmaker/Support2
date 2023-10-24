f46b143eca476eec3a35fa054fda8050629ea69d
Maxiu <Maxiu@DESKTOP-H8L1RT4>
2023-10-24 19:26:10 +0200

    Dodaj pliki projektów.

---
+++ Support2/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Support2.Models;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Support2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new LoginGet();
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult logowanie(LoginGet model)
+        {
+            if (model.login == "admin" && model.password == "admin")
+            {
+                return View("demo");
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+        public IActionResult demo()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Support2/Models/LoginGet.cs
@@ -0,0 +1,8 @@
+﻿namespace Support2.Models
+{
+    public class LoginGet
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+}
