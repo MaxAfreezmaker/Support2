82414a36897f3ac96bd341b5429227721243e02e
Maxiu <Maxiu@DESKTOP-H8L1RT4>
2023-10-27 00:05:35 +0200

    Koniec Prac

--- Support2/Controllers/SupportController.cs
+++ Support2/Controllers/SupportController.cs
@@ -21,66 +21,53 @@ namespace Support2.Controllers
             {
                 if (_context != null)
                 {
-                    // Wykonaj jakieś operacje związane z _context, na przykład, pobierz dane
                     var zgloszenial = _context.zgloszenia.ToList();
-                   // return View(zgloszenial);
-                    if (zgloszenial != null )
+
+                    if (zgloszenial != null && zgloszenial.Any())
                     {
-                        List<zgloszenial> zgloszeniadane = new List<zgloszenial>();
-                        foreach ( var z in zgloszenial)
-                        {
-                            var zgloszenial1 = new zgloszenial()
-                            {
-                                ID = z.ID,
-                                Imie = z.Imie,
-                                Nazwisko = z.Nazwisko,
-                                opis = z.opis,
-                                Typ_zgloszenia = z.Typ_zgloszenia
-                            };
-                            return View("Home/LogginIn/Demo", zgloszenial);
-                        }
-                        return View("Home/LogginIn/Demo", zgloszenial);
+                        return View("LogginIn/Demo", zgloszenial);
                     }
                     else
                     {
-                        return View("BrakDostepnychRekordow");
+                        return View("LogginIn/BrakDostepnychRekordow");
                     }
                 }
                 else
                 {
-                    // Obsłuż sytuację, gdy _context jest nullem
-                    return View("Error"); // Możesz utworzyć widok obsługujący błędy
+                    
+                    return View("Error");
                 }
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Błąd podczas pobierania danych z bazy danych");
-                return View("Error"); 
+                return View("Error");
             }
-            /*
-                      var zgloszeniaall = _context.zgloszenia.ToList();
-                       List<zgloszenial> zgloszenias = new List<zgloszenial>();
-                       if (zgloszeniaall != null)
-                       {
-
-                           foreach(var zgloszenial in zgloszeniaall)
-                           {
-                               var zgloszeniaas = new zgloszenial()
-                               {
-                                   ID = zgloszenial.ID,
-                                   Imie = zgloszenial.Imie,
-                                   Nazwisko = zgloszenia;.Nazwisko,
-                                   Typ_zgloszenia = zgloszenial.Typ_zgloszenia,
-                                   Tresc_zgloszenia = zgloszenial.Tresc_zgloszenia
-                               };
-                               zgloszenias.Add(zgloszenial);
-                           }
-
-                           return View(zgloszenias);
-                       }
-              
-            return View();
-            */
         }
+        /*
+                  var zgloszeniaall = _context.zgloszenia.ToList();
+                   List<zgloszenial> zgloszenias = new List<zgloszenial>();
+                   if (zgloszeniaall != null)
+                   {
+
+                       foreach(var zgloszenial in zgloszeniaall)
+                       {
+                           var zgloszeniaas = new zgloszenial()
+                           {
+                               ID = zgloszenial.ID,
+                               Imie = zgloszenial.Imie,
+                               Nazwisko = zgloszenia;.Nazwisko,
+                               Typ_zgloszenia = zgloszenial.Typ_zgloszenia,
+                               Tresc_zgloszenia = zgloszenial.Tresc_zgloszenia
+                           };
+                           zgloszenias.Add(zgloszenial);
+                       }
+
+                       return View(zgloszenias);
+                   }
+
+        return View();
+        */
     }
 }
+
